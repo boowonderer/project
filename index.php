b4fed334111aab3ddb80ed3214b2dc382dd202d4
Andrej-S-S <90258552+Andrej-S-S@users.noreply.github.com>
2021-12-07 13:51:46 +0100

    TEST

--- index.php
+++ index.php
@@ -115,6 +115,21 @@ XYZ;
         </div>
     </div>
 </div>
+<section-wave id="about">
+        <div class="content2">
+            <h2>About Us Excido</h2>
+            <p>
+                Escape Room Excido is a brand new escape room experience in Southeast Michigan.
+
+                We know all escape rooms are slightly different but the idea is the same, get locked in and try to get out in under an hour. So we wanted to make ours really stand out.
+
+                You decide what kind of adventure you want with multiple rooms that can be different levels of difficulty in each room. Our rooms are fully themed with interactive special effects that take you to another world or a different time and place. We can’t give away all our secrets but we promise you a one-of-a-kind experience that is a challenging and memorable good time. You leave with hilarious and great stories about how much fun you had and how you did or didn’t escape the room.
+            </p>
+        </div>
+        <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#fff" fill-opacity="1" d="M0,128L48,149.3C96,171,192,213,288,202.7C384,192,480,128,576,133.3C672,139,768,213,864,229.3C960,245,1056,203,1152,181.3C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+        </svg>
+    </section-wave>
 
 <div>
     <h2 class="score"> Scores</h2>
